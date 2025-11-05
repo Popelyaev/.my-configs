06a9659b3e16af5b8f332b4d17fa78aacf9aa6ab
Evgeniy Popelyaev <webpixel-development@yandex.ru>
2025-11-06 02:56:19 +0700

    vim: скорректирован скрипт по добавлению ссылок

--- vim/vim.links.sh
+++ vim/vim.links.sh
@@ -5,4 +5,9 @@ REPO_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 # Символические ссылки
 ln -sf "$REPO_DIR/vimrc" ~/.vimrc
-ln -sf "$REPO_DIR/vim" ~/.vim
+
+if [ -d "$HOME/.vim" ] || [ -L "$HOME/.vim" ]; then
+  rm -rf "$HOME/.vim"
+fi
+
+ln -s "$REPO_DIR/vim" ~/.vim
