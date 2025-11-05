18dcc67d83fbcadd4702a58d216395cd1b95159d
Evgeniy Popelyaev <webpixel-development@yandex.ru>
2025-11-06 03:26:34 +0700

    alacritty: добавлен конфиг и скрит ссылок

---
+++ alacritty/alacritty/shell/opacity_off.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ~/.config/alacritty/configs/alacritty_standart.toml ~/.config/alacritty/alacritty.toml
+# pkill -USR1 alacritty
---
+++ alacritty/alacritty/shell/opacity_on.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ~/.config/alacritty/configs/alacritty_transparent.toml ~/.config/alacritty/alacritty.toml
+# pkill -USR1 alacritty
